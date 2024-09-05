9e4fdf6f82bd856563fda909cc33212fe80a5189
nooby6 <giftaudrine1@gmail.com>
2024-09-05 21:54:33 +0300

    Refactor layout.tsx: Update body class name and add background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from '/@lib/utils';
+import { cn } from '@/lib/utils';
 
 const fontSans = Plus_Jakarta_Sans({ 
   subsets: ["latin"], 
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300')}>{children}</body>
     </html>
   );
 }
