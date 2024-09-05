77da4ad9e2fd16aca96bb85d04c69e20e48f8dba
nooby6 <giftaudrine1@gmail.com>
2024-09-05 21:47:10 +0300

    Refactor layout.tsx: Import 'cn' utility function

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
+import { cn } from '/@lib/utils';
+
 const fontSans = Plus_Jakarta_Sans({ 
   subsets: ["latin"], 
   weight: ['300', '400', '500', '600', '700'],
