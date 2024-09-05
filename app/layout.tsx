0cc7624cb5d09457d65962274bcb268ed4e9b3d1
nooby6 <giftaudrine1@gmail.com>
2024-09-05 21:44:17 +0300

    Refactor layout.tsx: Update font import and class name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = Plus_Jakarta_Sans({ 
+  subsets: ["latin"], 
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-sans'
+});
 
 export const metadata: Metadata = {
   title: "CarePulse",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn}>{children}</body>
     </html>
   );
 }
