3b315216bebd2638cebf497cae9b6dd2aa3dd586
nooby6 <giftaudrine1@gmail.com>
2024-09-09 17:01:56 +0300

    Refactor Home component: Add Next image component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Image } from "next/image"
 
 export default function Home() {
   return (
    <div className="flex h-screen maxh-screen">
     <section className="remove-scrollbar container my-auto">
-
+      <div className="sub-container max-w-[496px]"
+        <Image />
+      </div>
     </section>
-   </div>
+  </div>
   );
 }
