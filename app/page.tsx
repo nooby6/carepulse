194d7755f75e3b3c2cc8725e5d9225ed3f4227ed
nooby6 <giftaudrine1@gmail.com>
2024-09-11 22:08:37 +0300

    Refactor Home component: Update Next image import and add alt text

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@/components/ui/button";
-import { Image } from "next/image"
+import Image from "next/image"
 
 export default function Home() {
   return (
    <div className="flex h-screen maxh-screen">
     <section className="remove-scrollbar container my-auto">
-      <div className="sub-container max-w-[496px]"
+      <div className="sub-container max-w-[496px]">
         <Image src="./assets/icons/logo-full.svg" 
         height={1000}
         width={1000}
+        alt="patient"
+        className="nb-12 h-10 w-fit"
         />
       </div>
     </section>
