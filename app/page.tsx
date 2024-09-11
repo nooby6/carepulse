fa63919e9b38ac020ed4df725b181013b4bbaba9
nooby6 <giftaudrine1@gmail.com>
2024-09-11 22:32:27 +0300

    Refactor Home component: Update image import and add alt text

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { Button } from "@/components/ui/button";
 import PatientForm from "@/components/ui/forms/PatientForm";
 import Image from "next/image"
-
 export default function Home() {
-  return (
-   <div className="flex h-screen maxh-screen">
+
     <section className="remove-scrollbar container my-auto">
       <div className="sub-container max-w-[496px]">
         <Image 
@@ -16,8 +14,10 @@ export default function Home() {
         />
 
         <PatientForm/>
+
+        <div className="text-14-regular"></div>
+
       </div>
     </section>
-  </div>
-  );
+  )
 }
