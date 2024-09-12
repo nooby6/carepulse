b0ac1a8f6e270b35d942d24323486d2dd4a46020
nooby6 <giftaudrine1@gmail.com>
2024-09-12 19:39:58 +0300

    Refactor Home component: Update image import and add alt text

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,48 @@
-import { Button } from "@/components/ui/button";
-import PatientForm from "@/components/ui/forms/PatientForm";
-import Image from "next/image"
-export default function Home() {
+import Image from "next/image";
+import Link from "next/link";
 
-    <section className="remove-scrollbar container my-auto">
-      <div className="sub-container max-w-[496px]">
-        <Image 
-        src="/assets/icons/logo-full.svg" 
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
         height={1000}
         width={1000}
         alt="patient"
-        className="nb-12 h-10 w-fit"
-        />
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
+};
 
-        <PatientForm/>
-
-        <div className="text-14-regular"></div>
-
-      </div>
-    </section>
-  }
+export default Home;
