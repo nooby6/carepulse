610146f08a1272de5e04cc60943e9d04639f7084
nooby6 <giftaudrine1@gmail.com>
2024-09-12 20:29:52 +0300

    Refactor: Update import path for PasskeyModal component in Home and remove unused GlobalError component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { PatientForm } from "@/components/forms/PatientForm";
+import PatientForm from "../components/forms/PatientForm";
 //import { PasskeyModal } from "../components/PasskeyModal"; */
 
 const Home = ({ searchParams }: SearchParamProps) => {
@@ -9,7 +9,7 @@ const Home = ({ searchParams }: SearchParamProps) => {
 
   return (
     <div className="flex h-screen max-h-screen">
-      {isAdmin && <PasskeyModal />}
+    {/* {isAdmin && <PasskeyModal />} */}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
