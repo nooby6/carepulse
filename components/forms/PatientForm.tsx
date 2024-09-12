f9a2e27e11ed1bba63011fa6c4c48ca0744e83c6
nooby6 <giftaudrine1@gmail.com>
2024-09-12 21:39:28 +0300

    Refactor: Update minimum character requirement for username in PatientForm

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,7 +6,7 @@ import { z } from "zod"
  
 const formSchema = z.object({
   username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+    message: "Username must be at least 4 characters.",
   }),
 })
  
