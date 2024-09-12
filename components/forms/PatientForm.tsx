4fb7f5c5f20ee333e0c02ffacb7ee6b813d937c9
nooby6 <giftaudrine1@gmail.com>
2024-09-12 21:41:34 +0300

    Refactor: Update import paths and remove unused components in PatientForm

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,6 +3,17 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
  
 const formSchema = z.object({
   username: z.string().min(2, {
