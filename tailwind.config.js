2a317daff1147aa9e9539fbe9af1e87f87fd908f
nooby6 <giftaudrine1@gmail.com>
2024-09-17 15:25:05 +0300

    Refactor: Update layout and tailwind configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}", // Scans the pages directory for Tailwind class names
+    "./components/**/*.{js,ts,jsx,tsx}", // Scans the components directory
+    "./app/**/*.{js,ts,jsx,tsx}", // Scans the app directory if you're using the new app structure
+  ],
   theme: {
     extend: {},
   },
