4369b95f5bb1c227f70a3d09eeed07cbb54d6b6a
nooby6 <giftaudrine1@gmail.com>
2024-09-18 22:29:15 +0300

    Refactor: Update Tailwind configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}", // Scans the pages directory for Tailwind class names
-    "./components/**/*.{js,ts,jsx,tsx}", // Scans the components directory
-    "./app/**/*.{js,ts,jsx,tsx}", // Scans the app directory if you're using the new app structure
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
