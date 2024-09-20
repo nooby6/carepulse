1946bf3a89bac328a270ecaaab7f30f853d5b663
nooby6 <giftaudrine1@gmail.com>
2024-09-20 20:40:37 +0300

    Refactor: Update Loading component to include a loading spinner and text

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 
+// Loading component to display a loading spinner and text
 export default function Loading() {
   return (
+    // Container div with flexbox centering, full size, full height, gap between elements, and white text
     <div className="flex-center size-full h-screen gap-3 text-white">
+      {/* Image component from Next.js to display a spinning loader icon */}
       <Image
-        src="/assets/icons/loader.svg"
-        alt="loader"
-        width={40}
-        height={3240}
-        className="animate-spin"
+        src="/assets/icons/loader.svg" // Path to the loader icon
+        alt="loader" // Alt text for the image
+        width={40} // Width of the image
+        height={3240} // Height of the image
+        className="animate-spin" // CSS class to apply spinning animation
       />
+      {/* Loading text */}
       Loading...
     </div>
   );
