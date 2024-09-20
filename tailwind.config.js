961f9ac9cd27dd79e0ddb8119e750b02993cd58c
nooby6 <giftaudrine1@gmail.com>
2024-09-20 20:53:31 +0300

    Refactor: Update tailwind.config.js to include template file paths for pages, components, and app directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Specify the paths to all of the template files in your project
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}", // Include all JS, TS, JSX, and TSX files in the pages directory
+    "./components/**/*.{js,ts,jsx,tsx}", // Include all JS, TS, JSX, and TSX files in the components directory
+    "./app/**/*.{js,ts,jsx,tsx}", // Include all JS, TS, JSX, and TSX files in the app directory
   ],
   theme: {
+    // Extend the default Tailwind CSS theme
     extend: {},
   },
+  // Add any plugins here
   plugins: [],
 };
